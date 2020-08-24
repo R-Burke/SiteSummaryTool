ef6a5b2caa69780fcb30e427970419bf1a4f41c4
Rachel Burke <51931055+R-Burke@users.noreply.github.com>
2020-08-24 14:26:27 -0600

    Update InstallPackages.R

--- R/InstallPackages.R
+++ R/InstallPackages.R
@@ -3,15 +3,15 @@ InstallPackages <- function(LibraryPath){
   myPaths <- .libPaths()
   myPaths <- c(myPaths, LibraryPath)
 
-  packages <- c("devtools", "tidyverse", "knitr", "DT",
+  packages <- c("devtools", "tidyverse", "knitr", "DT", "leaflet", 
                 "sf", "rworldmap", "rworldxtra", "plotly", "htmltools",
-                "http://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_3.2.1.tar.gz")
+                "ggplot2")
 
   package.check <- lapply(
     packages,
     FUN = function(x) {
       if (!require(x, character.only = TRUE)) {
-        install.packages(x, dependencies = TRUE, type = "source")
+        install.packages(x, dependencies = TRUE)
         library(x, character.only = TRUE)
       }
     }
