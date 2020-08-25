ea3bc2acd634303a52e72698ccbbb3d38f2f282d
Rachel Burke <51931055+R-Burke@users.noreply.github.com>
2020-08-25 10:50:51 -0600

    Update ReadSpeciesList.R

--- R/ReadSpeciesList.R
+++ R/ReadSpeciesList.R
@@ -4,10 +4,5 @@ ReadSpeciesList <- function(TerrADat_Path, Internal){
     SpeciesList<- sf::st_read(dsn = TerrADat_Path ,  
                               layer = "tblStateSpecies")}
   if(Internal){
-    conn <- RODBC::odbcConnect("ilmocAIMPub")
-    SpeciesList <- sqlQuery(conn, 'SELECT * FROM ilmocAIMPub.ilmocAIMPubDBO.tblStateSpecies;')}
-  
-  SpeciesList <- SpeciesList %>% dplyr::rename(Species = SpeciesCode) 
-  return(SpeciesList)
-
+    SpeciesList <- SpeciesList
   }
