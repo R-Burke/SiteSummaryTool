0556df95f73876857c7cb8704fb8ece33b40972b
Rachel Burke <51931055+R-Burke@users.noreply.github.com>
2020-09-03 15:19:50 -0600

    Update SubsetEcologicalSite.R

--- R/SubsetEcologicalSite.R
+++ R/SubsetEcologicalSite.R
@@ -8,11 +8,3 @@ SubsetEcologicalSite <- function(TDat_LMF, EcologicalSiteId){
   return(EcoSitePlots)
 }
 
-SubsetEcologicalSite_Species <- function(EcoSitePlots, Species_Indicator){
-  
-  EcoSite_PKs <- EcoSitePlots$PrimaryKey
-  Species_plots_ecosite <- Species_Indicator[(Species_Indicator[["PrimaryKey"]] %in% EcoSite_PKs), ]
-  
-  return(Species_plots_ecosite)
-  
-}
