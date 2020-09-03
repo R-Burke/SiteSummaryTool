114ff5983198577c2d34116712fd7c213901a199
Rachel Burke <51931055+R-Burke@users.noreply.github.com>
2020-09-03 15:05:13 -0600

    Update SubsetEcologicalSite.R

--- R/SubsetEcologicalSite.R
+++ R/SubsetEcologicalSite.R
@@ -3,7 +3,7 @@ SubsetEcologicalSite <- function(TDat_LMF, EcologicalSiteId){
   EcoSitePlots <- TDat_LMF[TDat_LMF[["EcologicalSiteId"]] %in% EcologicalSiteId, ]
 # Add sagebrush indicator
   EcoSitePlots <- EcoSitePlots %>%
-                  mutate(AH_SagebrushCover_Dead = AH_SagebrushCover - AH_SagebrushCover_Live)
+                  dplyr::mutate(AH_SagebrushCover_Dead = AH_SagebrushCover - AH_SagebrushCover_Live)
    
   return(EcoSitePlots)
 }
