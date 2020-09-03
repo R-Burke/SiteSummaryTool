d04c451d6d06c973ee091a325a3d9585bb0a9bbb
Rachel Burke <51931055+R-Burke@users.noreply.github.com>
2020-09-03 15:20:06 -0600

    Delete SubsetEcologicalSite_Species.R

--- R/SubsetEcologicalSite_Species.R
+++
@@ -1,8 +0,0 @@
-SubsetEcologicalSite_Species <- function(EcoSitePlots, Species_Indicator){
-  
-  EcoSite_PKs <- EcoSitePlots$PrimaryKey
-  Species_plots_ecosite <- Species_Indicator[(Species_Indicator[["PrimaryKey"]] %in% EcoSite_PKs), ]
-  
-  return(Species_plots_ecosite)
-  
-}
