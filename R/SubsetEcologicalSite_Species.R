da72d0551fa578165efb7dfa5db36322b1c821ca
Rachel Burke <51931055+R-Burke@users.noreply.github.com>
2020-09-04 16:26:53 -0600

    Update SubsetEcologicalSite_Species.R

--- R/SubsetEcologicalSite_Species.R
+++ R/SubsetEcologicalSite_Species.R
@@ -1,5 +1,5 @@
 
-SubsetEcologicalSite_Species <- function(EcoSitePlots, Species_Indicator){}
+SubsetEcologicalSite_Species <- function(EcoSitePlots, Species_Indicator){
   
   EcoSite_PKs <- EcoSitePlots$PrimaryKey
   Species_plots_ecosite <- Species_Indicator[(Species_Indicator[["PrimaryKey"]] %in% EcoSite_PKs), ]
